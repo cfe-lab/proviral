be06bd8a5fd49f261986a331717748828a6ee57b
dan.msi <dan.sendbot@gmail.com>
2020-07-14 10:51:13 -0700

    Added R configuration script

---
+++ gene_splicer/configure_r.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+local({r <- getOption("repos")
+    r["CRAN"] <- "https://cloud.r-project.org"
+    options(repos=r)
+})
+
+message('======== INSTALLING BIOSTRINGS ========')
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+BiocManager::install("Biostrings")
+
+message('======== INSTALLING MUSCLE ========')
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+BiocManager::install("muscle")
