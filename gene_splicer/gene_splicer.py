b0e2ebc7066ef5088f7f8f9ca7530421e9927557
dan.msi <dan.sendbot@gmail.com>
2020-09-01 17:13:13 -0700

    Moved function outside of loop

--- gene_splicer/gene_splicer.py
+++ gene_splicer/gene_splicer.py
@@ -9,9 +9,16 @@ def parse_args():
     parser = argparse.ArgumentParser(
         description='Splice out HIV genes from a sequence',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('query_fasta', help='A fasta file containing one or more query sequences')
-    parser.add_argument('--name', help='A name for your analysis', default='default_name')
-    parser.add_argument('--outdir', type=Path, default=Path(os.getcwd()).resolve(), help='Path to output files')
+    parser.add_argument(
+        'query_fasta',
+        help='A fasta file containing one or more query sequences')
+    parser.add_argument('--name',
+                        help='A name for your analysis',
+                        default='default_name')
+    parser.add_argument('--outdir',
+                        type=Path,
+                        default=Path(os.getcwd()).resolve(),
+                        help='Path to output files')
     return parser.parse_args()
 
 
@@ -19,25 +26,24 @@ def run(query_fasta, name, outdir):
     target = utils.mod_hxb2
     for query_name, query_seq in utils.read_fasta(query_fasta):
         # Splitting by '::' is quite specific, make sure primer_finder joins using this
-        samfile_path = utils.align(target, query_seq, query_name[1:].split('::')[1], outdir=outdir)
+        samfile_path = utils.align(target,
+                                   query_seq,
+                                   query_name[1:].split('::')[1],
+                                   outdir=outdir)
         samfile = utils.load_samfile(samfile_path)
-        results = utils.splice_genes(query_seq, target, samfile, utils.mod_annot)
+        results = utils.splice_genes(query_seq, target, samfile,
+                                     utils.mod_annot)
         genes = utils.coords_to_genes(results, query_seq)
         genes_path = samfile_path.parent / 'genes.fasta'
         with open(genes_path, 'w') as o:
             for gene, seq in genes.items():
                 o.write(f'>{gene}\n{seq}\n')
-    utils.generate_table_precursor(name, outdir)
 
 
 def main():
     args = parse_args()
     logger.debug(args)
-    run(
-        query_fasta=args.query_fasta,
-        name=args.name,
-        outdir=args.outdir
-    )
+    run(query_fasta=args.query_fasta, name=args.name, outdir=args.outdir)
 
 
 if __name__ == '__main__':
--- gene_splicer/pipeline.py
+++ gene_splicer/pipeline.py
@@ -5,7 +5,11 @@ import primer_finder
 def main():
     data = primer_finder.main()
     for _file in data['fasta_files']:
-        gene_splicer.run(_file, name=data['args'].name, outdir=data['args'].outpath)
+        gene_splicer.run(_file,
+                         name=data['args'].name,
+                         outdir=data['args'].outpath)
+    utils.generate_table_precursor(name=data['args'].name,
+                                   outdir=data['args'].outpath)
 
 
 if __name__ == '__main__':
