3f22e0ca4624f2edc81a0da7f825320087d99ddc
dan.msi <dan.sendbot@gmail.com>
2021-02-08 14:07:08 -0800

    Fixed proviral helper to actually load csv

--- gene_splicer/helpers/proviral_helper.py
+++ gene_splicer/helpers/proviral_helper.py
@@ -1,7 +1,9 @@
 import yaml
 import os
+import csv
 from pathlib import Path
 import helpers.yaml_helper
+from logger import logger
 
 
 # force_all_proviral is for testing, forces samples to be considered proviral
@@ -9,7 +11,8 @@ class ProviralHelper:
     def __init__(self, force_all_proviral=False) -> None:
         self.force_all_proviral = force_all_proviral
         self.cwd = self.getcwd()
-        self.load_config()
+        self.config = self.load_config()
+        self.proviral_samples = self.load_proviral_samples()
 
     @staticmethod
     def getcwd():
@@ -17,14 +20,23 @@ class ProviralHelper:
 
     def load_config(self):
         with open(self.cwd.parent / 'config.yaml') as o:
-            self.config = yaml.load(o, Loader=yaml.Loader)
+            return yaml.load(o, Loader=yaml.Loader)
 
-    def get_proviral_samples(self):
-        return self.config['RESOURCES']['PROVIRAL_SAMPLES']
+    def load_proviral_samples(self):
+        apath = self.config['RESOURCES']['PROVIRAL_SAMPLES']
+        proviral_samples = {}
+        with open(apath, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                if row['is_proviral'] == 'TRUE':
+                    proviral_samples[row['sample']] = row
+        return proviral_samples
 
     def is_proviral(self, sample):
-        return sample in self.config['RESOURCES'][
-            'PROVIRAL_SAMPLES'] or self.force_all_proviral
+        return sample in self.proviral_samples or self.force_all_proviral
 
-    def get_sample_pid_mapping(self):
-        return self.config['RESOURCES']['SAMPLE_PID_MAPPING']
+    def pid_from_sample(self, sample):
+        try:
+            return self.proviral_samples[sample]['Category']
+        except KeyError:
+            logger.warning('Sample %s not in proviral samples file' % (sample))
