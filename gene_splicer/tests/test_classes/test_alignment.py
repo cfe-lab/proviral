28a06d15d7060cb42d2e5ea1f96e9dce630c88f3
Don Kirkby <donkirkby@gmail.com>
2021-04-01 15:51:34 -0700

    Remove a broken test, and clean up temp files.

--- gene_splicer/tests/test_classes/test_alignment.py
+++ gene_splicer/tests/test_classes/test_alignment.py
@@ -3,7 +3,6 @@ import sys
 from pathlib import Path
 
 cwd = Path(os.path.realpath(__file__)).parent
-data = cwd.parent / 'data'
 sys.path.append(str(cwd.parent.parent))
 
 import gene_splicer.utils as utils
@@ -11,9 +10,10 @@ from fasta import Fasta
 from gene_splicer.alignment import Alignment
 
 
-def test_alignment():
+def test_alignment(tmp_path):
+    data = cwd.parent / 'data'
     example = data / 'example2'
-    valid_aln = utils.load_samfile(example / 'alignment.sam')
+    utils.load_samfile(example / 'alignment.sam')
     # Read in the target sequence
     target = None
     for header, seq in Fasta(example / 'target.fasta'):
@@ -22,7 +22,7 @@ def test_alignment():
     query = None
     for header, seq in Fasta(example / 'query.fasta'):
         query = seq
-    aln = Alignment(target, query, cwd / 'tmp')
+    aln = Alignment(target, query, tmp_path)
     test_aln = utils.load_samfile(aln.path)
     valid_aln = utils.load_samfile(example / 'alignment.sam')
     assert test_aln.equals(valid_aln)
--- gene_splicer/tests/test_probe_finder/test_probe_finder.py
+++ gene_splicer/tests/test_probe_finder/test_probe_finder.py
@@ -3,8 +3,6 @@ import pytest
 from pathlib import Path
 
 from gene_splicer.probe_finder import ProbeFinder
-import gene_splicer.utils as utils
-from gene_splicer.fasta import Fasta
 
 
 @pytest.fixture
@@ -19,16 +17,3 @@ def test_probe_finder(currentdir):
     query = 'TCGA'
     finder = ProbeFinder(query, target)
     assert finder.contig_match == 'TCGA'
-
-
-def test_real_example1(currentdir):
-    example = currentdir / 'ignore_example1'
-    fasta = Fasta(example / 'query.fasta')
-    for h, s in fasta:
-        query = s
-    target = utils.mod_hxb2
-    aln_path = example / 'alignment.sam'
-    coords = utils.sequence_to_coords(query, target, aln_path, utils.mod_annot)
-    genes = utils.coords_to_genes(coords, query)
-    assert 'x1' in genes
-    assert genes['x1'] == 'AGAAACCAGAGGAGCTCTCTCGACGCAGGACTCG'
