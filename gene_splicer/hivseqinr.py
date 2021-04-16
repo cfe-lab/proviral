63345d40d396543554815dee126b630ac4c36aff
Don Kirkby <donkirkby@gmail.com>
2021-04-16 14:04:46 -0700

    Append positive controls to sample data, for #5.
    Also copy in some missing data files from the HIVSeqinR source code to the working folder.

--- gene_splicer/hivseqinr.py
+++ gene_splicer/hivseqinr.py
@@ -9,7 +9,9 @@ import zipfile
 import io
 
 logger = logging.getLogger(__name__)
-SOURCE_URL = 'https://github.com/guineverelee/HIVSeqinR/raw/master/HIVSeqinR_ver2.7.1.zip'
+SOURCE_URL = 'https://github.com/guineverelee/HIVSeqinR/raw/master/'
+SOURCE_ZIP = 'HIVSeqinR_ver2.7.1.zip'
+REFERENCE_FILE = 'Examples_8E5_HXB2.fasta'
 
 
 class Hivseqinr:
@@ -21,15 +23,24 @@ class Hivseqinr:
 
     def copy_fasta(self):
         raw_fastas_path = self.outpath / 'RAW_FASTA'
-        try:
-            os.makedirs(raw_fastas_path)
-        except FileExistsError:
-            pass
+        raw_fastas_path.mkdir(exist_ok=True)
+        target_path = raw_fastas_path / self.fasta.name
         logger.debug('Attempting to copy "%s" to "%s"' %
                      (self.fasta, raw_fastas_path))
-        shutil.copy(self.fasta, raw_fastas_path)
-        shutil.copy(self.source_path / 'R_HXB2.fasta',
-                    self.outpath / 'R_HXB2.fasta')
+        shutil.copy(self.fasta, target_path)
+
+        # Add positive control sequences to the end of the samples.
+        reference_path = self.source_path / REFERENCE_FILE
+        with reference_path.open() as ref_file, \
+                target_path.open('a') as fasta_file:
+            shutil.copyfileobj(ref_file, fasta_file)
+
+        source_files = ['R_HXB2.fasta',
+                        'HIV Genome RefSeq ver04 NUC.fasta',
+                        'HIV Genome RefSeq ver04 AA.fasta']
+        for source_file in source_files:
+            shutil.copy(self.source_path / source_file,
+                        self.outpath / source_file)
 
     def make_blast_dir(self):
         self.blast_db.mkdir(exist_ok=True)
@@ -47,12 +58,15 @@ class Hivseqinr:
         license_path = self.source_path / 'LICENSE.txt'
         if license_path.exists():
             return
-        response = requests.get(SOURCE_URL)
+        response = requests.get(SOURCE_URL + SOURCE_ZIP)
         file_like_object = io.BytesIO(response.content)
         zipfile_obj = zipfile.ZipFile(file_like_object)
         if not os.path.isdir(self.source_path):
             os.makedirs(self.source_path)
         zipfile_obj.extractall(self.source_path)
+        response = requests.get(SOURCE_URL + REFERENCE_FILE)
+        reference_path = self.source_path / REFERENCE_FILE
+        reference_path.write_bytes(response.content)
         self.make_blast_dir()
         self.fix_wds()
 
