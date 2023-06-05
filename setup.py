7cfd902c61efde1d4f4704fae23bac21768fb562
Vitaliy Mysak <vmysak@bccfe.ca>
2023-06-05 12:58:56 -0700

    Add HIVIntact to our python dependencies

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
         'python-Levenshtein==0.12.0',
         'pandas==2.0.2',
         'requests==2.31.0',
+        'intactness-pipeline @ git+https://github.com/cfe-lab/HIVIntact.git@cfe-1.2',
         'pyyaml'
     ],
     package_data={
