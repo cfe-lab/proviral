054dbaa82f4f19ec870fb4117dad587df6bf09a5
Vitaliy Mysak <vmysak@bccfe.ca>
2025-06-24 19:58:01 +0000

    Bump project version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cfeproviral',
-    version='v2.4.3',
+    version='v2.4.4',
     classifiers=[
         'License :: OSI Approved :: MIT License',
     ],
