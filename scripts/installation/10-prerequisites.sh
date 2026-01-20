da53e553dd638133a77e9e542174470c1f85c408
Vitaliy Mysak <vmysak@bccfe.ca>
2026-01-20 00:31:58 +0000

    Ensure /w exists

--- scripts/installation/10-prerequisites.sh
+++ scripts/installation/10-prerequisites.sh
@@ -6,6 +6,9 @@ echo ===== Installing Prerequisites ===== >/dev/null
 
 set -x
 
+mkdir -p /w
+cd /w
+
 apt-get update -qq
 apt-get install -y build-essential unzip git wget \
         fontconfig libbz2-dev liblzma-dev libssl-dev \
